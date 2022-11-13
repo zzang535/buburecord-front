e719c9377adae880390b6e9c3781ad28f253aaab
june hwang <zzang535@gmail.com>
2022-11-13 15:19:58 +0900

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 module.exports = {
     apps: [
       {
-        name: `${process.env.APP_ENVIRONMENT}.seoteacher`,
+        name: `${process.env.APP_ENVIRONMENT}.album.bird89`,
         exec_mode: 'cluster',
         instances: 'max', // Or a number of instances
         script: './node_modules/nuxt/bin/nuxt.js',
