39f6175e78a9551602c73bfdff1ea465cca21c22
zzang535 <zzang535@gmail.com>
2024-05-03 13:29:14 +0900

    eslint

--- store/index.js
+++ store/index.js
@@ -1,29 +1,30 @@
-export const strict = false
+export const strict = false;
 
 const USER_DATA = () => {
-    return {
-        id: null,
-        username: ''
-    }
-} 
+  return {
+    id: null,
+    username: "",
+  };
+};
 
 export const state = () => ({
-    is_login: false,
-    user: {
-        id: null,
-        username: ''
-    }
-})
+  is_login: false,
+  user: {
+    id: null,
+    username: "",
+  },
+});
 
 export const mutations = {
-    login(state, payload) {
-        const user_data = payload
-        state.user.id = user_data.id
-        state.user.username = user_data.username
-        state.is_login = true
-    },
-    logout(state){
-        state.user = USER_DATA()
-        state.is_login = false
-    }
-}
+  login(state, payload) {
+    console.log("payload", payload);
+    const user_data = payload;
+    state.user.id = user_data.id;
+    state.user.username = user_data.username;
+    state.is_login = true;
+  },
+  logout(state) {
+    state.user = USER_DATA();
+    state.is_login = false;
+  },
+};
